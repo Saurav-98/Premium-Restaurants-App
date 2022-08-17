3d810de2157261c834d578593fa88df5cc2e4bc7
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-18 01:42:07 +0530

    Chefs Section Completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -2,10 +2,41 @@ import React from 'react';
 
 import './Chef.css';
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+
+const Chef = () => {
+  return (
+    <div className="app__bg app__wrapper section__padding">
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+        <img src={images.chef} alt="chef" />
+      </div>
+
+      <div className="app__wrapper_info">
+        <SubHeading title="Chef's Word" />
+        <h1 className="headtext__cormorant">What we Believe in</h1>
+        <div className="app__chef-content">
+          <div className="app__chef-content-quote">
+            <img src={images.quote} alt="quote" />
+            <p className="p__opensans">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+          <p className="p__opensans">
+            Expedita at qui, beatae laboriosam voluptatem veritatis possimus,
+            neque optio, repellendus velit exercitationem provident ut. Quisquam
+            nostrum debitis ullam nesciunt ducimus expedita, vitae, culpa nam
+            id, necessitatibus sapiente odio eaque explicabo temporibus.
+          </p>
+        </div>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chef;
